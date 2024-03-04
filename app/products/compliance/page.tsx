aff5f2b612a0741eba4e9567bfeb82757d17bec4
unknown <ul0249604@edu.uni.lodz.eu>
2024-03-04 19:52:34 +0100

    fixed compliance section

--- app/products/compliance/page.tsx
+++ app/products/compliance/page.tsx
@@ -1,9 +1,10 @@
+import ComplianceInfo from '@/components/ComplianceInfo'
 import React from 'react'
 
 export default function page() {
     return (
     <>
-   
+      <ComplianceInfo />
     </>
     )
   } 
---
+++ components/ComplianceInfo.tsx
@@ -0,0 +1,148 @@
+import React from 'react'
+
+const ComplianceInfo = () => {
+  return (
+   <section className='mb-4'>
+        <div className='flex justify-left ml-20 mb-10 mt-6' >
+            <h1 className='text-blue-80 font-bold text-3xl lg:text-6xl '>Compliance Management Systems Ochrona sygnalistów</h1>
+            </div>
+
+         <div className='flex w-full max-h-[350px] justify-center px-20' >
+            <img src='/compliance_bg.jpg' alt='compliance' className='object-cover w-full rounded' />
+            </div>
+
+           
+            <div className='flex space-x-20 mb-4 px-16 justify-center'>
+
+  <div className='flex'>
+    <div className=' w-1/2 flex flex-col mt-4 rounded py-6'>
+
+      <div className='bg-gray-200 text-white flex flex-col mt-4 rounded px-8 py-6'>
+
+        <div className=''>
+        <p className='text-sky-40 text-justify text-m lg:text-l mb-4'><span className='font-bold'>Compliance Management System (System Zarządzanie Zgodnością) </span> to system ukierunkowany na samoregulację organizacji i zapewnienie jej prawidłowego funkcjonowania w zgodności z poniższymi regulacjami/normami</p>
+            <p className='mb-4'>➱ prawnymi, narzucanymi przez ustawodawcę i innymi zaleceniami organizacji regulacyjnych</p>  
+            <p className='mb-4'>➱ tycznymi i regułami postępowania stosowanymi powszechnie w sektorze (np. w sektorze jednostek finansów publicznych, takich jak jednostki samorządowe) i obrocie gospodarczym (w tym związanymi z zapobieganiem korupcji, dyskryminacji pracowników, konfliktem interesów i innymi podobnymi zagadnieniami)</p> 
+            <p className='mb-4'>➱ wynikającymi z przyjętej strategii zarządzania (normy tworzone wewnątrz danej organizacji w ramach jej dokumentacji organizacyjnej)</p> 
+        </div>
+
+        <div className='flex flex-col mt-12'>
+          <p className='text-sky-40 text-justify font-bold text-m lg:text-l mb-4  '>Cele compliance (zarządzania zgodnością):</p>
+            <p className='mb-4'>➱ ograniczenie ryzyka działalności, które w znacznej mierze dotyczy kierownictwa</p>  
+            <p className='mb-4'>➱ poprawa transparentności i zgodności działania z przyjętymi normami i zasadami postępowania, w celu zapobiegania stratom finansowym lub utracie reputacji</p> 
+            <p className='mb-4'>➱ wzmocnienie pozytywnego wizerunku organizacji w otoczeniu</p> 
+            <p className='mb-4'>➱ usprawnienia systemu nadzoru (transferu decyzji oraz współpracy) w organizacji</p> 
+            </div>
+
+
+            <div className='flex flex-col font-bold mt-12'>
+              <p className='text-sky-40 text-justify  text-m lg:text-l mb-4  '>Rekomendujemy wdrażanie compliance w oparciu o standardy światowe - głównie normę ISO 37301 „Compliance management systems”, a także:</p>
+                <p className='mb-4 text-white'>➱ ISO 37001 „Anti-bribery management systems” (Systemy zarządzania działaniami antykorupcyjnymi)</p>  
+                <p className='mb-4 text-white'>➱ ISO 37002 „Whistleblowing management systems” (Systemy zarządzania zgłaszaniem nieprawidłowości)</p> 
+          </div>
+          
+        </div>
+
+
+    
+     
+    </div>
+
+          <div className=' w-1/2 mt-4 rounded py-6'>
+              <div className=' flex flex-col bg-gray-500 ml-20 mt-4 rounded px-8 py-6 '>
+                <p className='text-sky-40 text-justify font-bold text-m lg:text-l  '>Systemy Zarządzania Zgodnością, które projektujemy cechuje nowoczesne podejście, integrujące zagadnienia dotyczące:</p>
+
+              <div className="flex flex-col justify-center mb-2 mt-8 text-white font-semibold text-3xl lg:text-base break-afrer-column  text-justify " >
+                      <p className='mb-4'>➱ zarządzania zgodnością z regulacjami zewnętrznymi i wewnętrznymi dotyczącymi działalności organizacji</p>  
+                      <p className='mb-4'>➱ przeciwdziałania korupcji</p>
+                      <p className='mb-4'>➱ zarządzania etyką (etycznym postępowaniem pracowników)</p> 
+                      <p className='mb-4'>➱ zgłaszania nadużyć, reagowania na zgłoszone nadużycia i ochrony sygnalistówm</p>
+                      <p className='mb-4'>➱ innych polityk istotnych dla integralnego (tj. prawego, uczciwego) funkcjonowania organizacji (takich jak: przeciwdziałanie konfliktowi interesów, dyskryminacji pracowników itp.), z uwzględnieniem <span className='font-bold text-sky-40 '>  ich zharmonizowania w wyniku doskonalenia ładu organizacyjnego (systemu organizacji i zarządzania) </span> dla zapewnienia synergicznego i efektywnego funkcjonowania organizacji (maksimum efektów i bezpieczeństwa przy minimum nakładów)</p>  
+                  </div>
+                  
+              </div>
+
+              <div className='flex flex-col bg-gray-500 ml-20 mt-4 rounded px-8 py-6'>
+              <p className='text-white text-justify font-semibold text-m lg:text-l  '>Poza praktycznymi względami proponowanego przez nas rozwiązania (zapobieganie korupcji i nieetycznym zachowaniom pracowników, doskonalenie organizacji jednostki), należy również podkreślić, że nasza oferta stanowi odpowiedź na najnowsze wymogi organów i instytucji regulacyjnych, formułowane względem polskich podmiotów (firm, jednostek samorządowych i innych), takie jak:</p>
+                <div className="flex flex-col justify-center mb-2 mt-8 text-sky-40 font-semibold text-3xl lg:text-base break-afrer-column  text-justify " >
+                        <p className='mb-4'>➱ Dyrektywa Parlamentu Europejskiego i Rady Europy (EU) 2019/1937 dotyczącą ochrony tzw. sygnalistów i związane z nią polskie akty prawne</p>
+                        <p className='mb-4'>➱ wytyczne Centralnego Biura Antykorupcyjnego dot. tworzenia i wdrażania efektywnych programów zgodności, a także jednolitych rozwiązań antykorupcyjnych w sektorze publicznym</p> 
+                    </div>
+              </div>
+
+          </div>
+      </div>
+      </div>  
+
+
+      <div className='flex w-full mt-4 max-h-[400px] justify-center px-8' >
+        <img src='/big_business2.jpg' alt='big company' className='object-cover w-full rounded' />
+      </div>
+
+      <div className='flex w-full mt-4 px-6'>
+
+        <div className='w-1/2 flex flex-col rounded py-6'>
+        <div className='bg-gray-200 flex flex-col rounded px-6 py-6'>
+          <p className='text-white text-justify font-bold text-m lg:text-l  '>Zakres prac:</p>
+          <p className='text-white text-justify font-semibold text-m lg:text-l mt-3'>Zakres każdego projektu dostosowywany jest indywidualnie do specyficznych potrzeb zamawiającego, ale jako przykład programu prac można wskazać przedstawione poniżej etapy.</p> 
+    
+              <div className='text-sky-40 font-semibold mt-5 px-6 py-6'>
+                  <ol className='flex flex-col list-decimal'>
+                      <li className='mb-4'> Punktem wyjścia jest audyt, ewaluacja i diagnoza silnych i słabych stron systemu organizacji i zarządzania w odniesieniu do światowych trendów i standardów oraz innych podmiotów o zbliżonym profilu działalności ze szczególnym uwzględnieniem:
+                          <span className='block ml-6 mt-2 text-white'>➱ kultury organizacyjnej (dominujący typ kultury vs. model określany na podstawie realizowanej strategii)</span>
+                          <span className='block ml-6 mt-2 text-white'>➱ konfiguracji struktur organizacyjnych</span>
+                          </li>
+
+                      <li className='mb-4'>W wyniku przeprowadzonych analiz zaprojektowane są: sformułowanie założeń dla systemu organizacji i zarządzania - filozofia zarządzania i model organizacyjno-biznesowy, opisujący strategię zarządzania poszczególnymi głównymi procesami gospodarczymi i obszarami funkcjonalnymi firmy, który stanowi układ katalityczny dla przyjętych celów działania firmy, oraz sprzyja wykorzystaniu szans i odparciu zagrożeń w otoczeniu.
+                          <span className='block ml-6 mt-2 text-white'>➱ struktura organizacyjna</span>
+                          <span className='block ml-6 mt-2 text-white'>➱ podział funkcji dla komórek/pracowników w układzie procesowym (proefektywnościowym) z uwzględnieniem kompetencji kierowniczych, wykonawczych oraz wsparcia, a także współpracy między komórkami organizacyjnymi</span>
+                          <span className='block ml-6 mt-2 text-white'>➱ dokumentacja organizacyjna/wdrożeniowa od regulaminu organizacyjnego do procedur i zakresów obowiązków pracowników.</span>
+                      </li>
+
+                      <li className='mb-4'>Po zakończeniu prac projektowych oferujemy wsparcie kierownictwa we wdrożeniu (szkolenia, konsultacje, wdrożenia systemów IT wspierających organizację w firmie) oraz bieżący audyt, konserwację i doskonalenie systemu organizacji i zarządzania (wprowadzanie zmian w dokumentacji, bieżący benchmarking procesów, wsparcie zmian kulturowych pracowników)</li>
+                  </ol>
+              </div>
+          </div>
+
+
+            <div className='flex flex-col bg-gray-500 mt-4 rounded px-8 py-6'>
+                    <p className='text-white text-justify font-semibold text-m lg:text-l  '>Projektując rozwiązania dedykowane dla naszych Klientów, dobieramy zakres regulacji do ich specyficznych potrzeb (od wdrażania wyłącznie procedur ochrony sygnalistów w pojedynczych podmiotach po kompleksowe, systemowe rozwiązania dla międzynarodowych grup kapitałowych).</p>
+                    <p className='text-white text-justify text-m lg:text-l mt-3 '>Specjalizujemy się  wdrażaniu systemów compliance  oraz związanych z nimi regulacji (np. polityki antykorupcyjne, procedury ochrony sygnalistów, programy etyczne) w następujących podmiotach:</p>
+
+                      <div className="flex flex-col justify-center mb-2 mt-4 text-sky-40 font-semibold text-3xl lg:text-base break-afrer-column  text-justify " >
+                              <p className='mb-4'>➱ spółki notowane na Giełdzie Papierów Wartościowych</p>
+                              <p className='mb-4'>➱ jednostki samorządowe (aby dowiedzieć się więcej zachęcamy do pobrania stosownej broszury)</p> 
+                        </div>
+                    </div>
+
+          </div>
+
+
+
+          <div className='w-1/2 flex flex-col rounded py-6'>
+
+                  <div className='flex flex-col ml-10' >
+                    <img src='/compliance2.jpg' alt='compliance' className='object-cover w-full rounded' />
+                 </div>
+
+                 <div className='flex flex-col bg-gray-500 mt-4 rounded px-8 py-6 ml-10'>
+                    <p className='text-white text-justify font-semibold text-m lg:text-l  '>Compliance i ochrona sygnalistów w urzędzie miasta</p>
+                    <p className='text-white text-justify text-m lg:text-l mt-3 '>Najnowsza publikacja Inorg pt. „RAZEM CZY OSOBNO? Trochę polemicznie na temat implementacji compliance w jednostkach samorządowych”</p>
+                    
+                      <div className="flex flex-col justify-center mb-2 mt-4 text-sky-40 font-semibold text-3xl lg:text-base break-afrer-column  text-justify " >
+                              <p className='mb-4'>Jak odpowiedzieć na wymagania europejskich i polskich przepisów dot. ochrony tzw. sygnalistów, efektywnych programów zgodności i jednolitych rozwiązań antykorupcyjnych?</p>
+                              <p className='mb-4'>Czy obserwowane od kilku lat turbulentne zmiany otoczenia prawnego i społeczno-ekonomicznego jednostek samorządowych wymagają stosowania niekonwencjonalnych rozwiązań organizacyjnych?</p> 
+                              <p className='mb-4'>Nasza odpowiedź na ww. pytania i nie tylko znajduje się w publikacji, do pobrania tutaj.</p>
+                        </div>
+                    </div>
+            </div>
+
+      </div>
+        
+      
+           
+    </section>
+  )
+}
+
+export default ComplianceInfo
