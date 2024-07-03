4f4b44e8f6241ea2cd161325edb8faf6d95330d8
unknown <ul0249604@edu.uni.lodz.eu>
2024-07-03 04:15:39 +0200

    added new photo to materials component

--- components/Materials.tsx
+++ components/Materials.tsx
@@ -8,8 +8,8 @@ const Materials = () => {
   return (
     <section className='mb-4 '>
         <div className="flex relative justify-center justify-left mb-4 mt-6  max-container padding-container" >
-        <CldImage src='https://res.cloudinary.com/dvbvaj9hu/image/upload/v1234/INORG/wspolpraca/vmpcm7yg3owkbfnj7clq.jpg' width={1900} height={1450} alt='branches' className='object-cover max-h-[450px]  w-full rounded' />
-            <h1 className='font-bold text-3xl xl:text-5xl 2xl:text-5xl absolute text-white tracking-widest' style={{ top: '70%', left: '8%' }}>{DATA?.MATERIALS1} <br/> {DATA?.MATERIALS2}</h1>
+        <CldImage src='https://res.cloudinary.com/dvbvaj9hu/image/upload/v1234/INORG/wspolpraca/zt2pcmk3dhq65kgjszyk.jpg' width={1900} height={1450} alt='branches' className='object-cover max-h-[450px]  w-full rounded' />
+            <h1 className='font-bold text-3xl xl:text-5xl 2xl:text-5xl absolute text-blue-80 tracking-widest' style={{ top: '65%', left: '8%' }}>{DATA?.MATERIALS1} <br/> {DATA?.MATERIALS2}</h1>
         </div>
         <div className='max-container padding-container '>
             <ul className='list-disc'>
