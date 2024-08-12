2307b6ae9c0df89c22bfc6423823dabaa75538a5
unknown <ul0249604@edu.uni.lodz.eu>
2024-08-12 18:40:45 +0200

    fixed dropdown lists from side menu and elements position with sidemenu open

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -14,6 +14,7 @@ const Navbar = () => {
 
   const handleLanguageChange = (lang: string) => {
     const selectedLanguage = LANGUAGES.find(language => language.code === lang);
+    setSideMenuOpen(false);
     setLocale(lang);
     setCurrentLanguage(selectedLanguage?.label || 'PL');
   };
@@ -28,12 +29,13 @@ const Navbar = () => {
     if (elem) {
       elem?.blur();
     }
+    setSideMenuOpen(false);
   };
 
   return (
-    <nav className="flex items-center max-container padding-container relative z-30 py-5">
-        <Link href="/" className="mr-10">
-        {currentLocale === 'pl' ? (  <CldImage src="https://res.cloudinary.com/dvbvaj9hu/image/upload/v1234/INORG/gbiuvxkdbtzfwwfzkz1a.svg" alt="INORG Logo" width={75} height={30} objectFit="cover"/>)
+    <nav className="flex items-start max-container padding-container relative z-30 py-5">
+        <Link href="/" className="mr-auto object-cover">
+        {currentLocale === 'pl' ? (  <CldImage src="https://res.cloudinary.com/dvbvaj9hu/image/upload/v1234/INORG/gbiuvxkdbtzfwwfzkz1a.svg" alt="INORG Logo" width={75} height={30} />)
       :
          ( <CldImage src="https://res.cloudinary.com/dvbvaj9hu/image/upload/v1234/INORG/wspolpraca/fbed3oncnbmwmwlwm6fy.svg" alt="INORG Logo" width={90} height={100} />)
       }
@@ -48,22 +50,14 @@ const Navbar = () => {
                 </Link>
               ) : link.key === 'recommendation' ?
               (<div key={link.label} className="dropdown dropdown-hover">
-                <div
-                  tabIndex={0}
-                  className="lg:text-nowrap text-base xl:text-base lg:font-bold text-default cursor-pointer mr-4"
-                >
+                <div tabIndex={0} className="lg:text-nowrap text-base xl:text-base lg:font-bold text-default cursor-pointer mr-4">
                   {link.label}
                 </div>
-                <ul
-                  tabIndex={0}
-                  className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-32"
+                <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-32"
                 >
                   {DATA?.RECOMMENDATIONS.map((recommendation, index) => (
                     <Link href={recommendation.href} key={index}>
-                      <li
-                        onClick={handleClick}
-                        className="text-wrap cursor-pointer text:md font-semibold h-12 sm:h-14 md:h-16 lg:h-14 xl:h-14 flex items-left justify-center"
-                      >
+                      <li onClick={handleClick} className="text-wrap cursor-pointer text:md font-semibold h-12 sm:h-14 md:h-16 lg:h-14 xl:h-14 flex items-left justify-center">
                         <p>{recommendation.label}</p>
                       </li>
                     </Link>
@@ -72,22 +66,13 @@ const Navbar = () => {
               </div>
               ) :
               (<div key={link.label} className="dropdown dropdown-hover">
-                <div
-                  tabIndex={0}
-                  className="lg:text-nowrap text-base xl:text-base lg:font-bold text-default cursor-pointer mr-4"
-                >
+                <div tabIndex={0} className="lg:text-nowrap text-base xl:text-base lg:font-bold text-default cursor-pointer mr-4">
                   {link.label}
                 </div>
-                <ul
-                  tabIndex={0}
-                  className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-44"
-                >
+                <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-44">
                   {DATA?.PRODUCTS.map((product, index) => (
                     <Link href={product.href} key={index}>
-                      <li
-                        onClick={handleClick}
-                        className="text-wrap cursor-pointer text:md font-semibold h-12 sm:h-14 md:h-16 lg:h-14 xl:h-14 flex items-left justify-center"
-                      >
+                      <li onClick={handleClick} className="text-wrap cursor-pointer text:md font-semibold h-12 sm:h-14 md:h-16 lg:h-14 xl:h-14 flex items-left justify-center">
                         <p>{product.name}</p>
                       </li>
                     </Link>
@@ -112,13 +97,45 @@ const Navbar = () => {
         {sideMenuOpen && (
          <div className="lg:hidden relative flex flex-col h-[calc(100vh-96px)] top-24 left-0 w-full justify-items-center items-center gap-12">
          
-              {DATA?.NAV_LINKS.map((link) => (
-                
-                <Link href={link.href} key={link.key} className= 'bold-10 text-default flexCenter cursor-pointer'>
+         {DATA?.NAV_LINKS.map((link:NavLink) => link.key !== 'products' && link.key !== 'recommendation' ? ( 
+                <Link key={link.label} href={link.href}  onClick={handleClick} > <div className="lg:text-nowrap text-base xl:text-base lg:font-bold text-default flexCenter cursor-pointer pb-1.5 transition-all mr-4">
                   {link.label}
+                  </div>
                 </Link>
-              ))}
-               <div className="dropdown dropdown-hover">
+              ) : link.key === 'recommendation' ?
+              (<div key={link.label} className="dropdown">
+                <div tabIndex={0} className="lg:text-nowrap text-base xl:text-base lg:font-bold text-default cursor-pointer mr-4">
+                  {link.label}
+                </div>
+                <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-32"
+                >
+                  {DATA?.RECOMMENDATIONS.map((recommendation, index) => (
+                    <Link href={recommendation.href} key={index}>
+                      <li onClick={handleClick} className="text-wrap cursor-pointer text:md font-semibold h-12 sm:h-14 md:h-16 lg:h-14 xl:h-14 flex items-left justify-center">
+                        <p>{recommendation.label}</p>
+                      </li>
+                    </Link>
+                  ))}
+                </ul>
+              </div>
+              ) :
+              (<div key={link.label} className="dropdown">
+                <div tabIndex={0} className="lg:text-nowrap text-base xl:text-base lg:font-bold text-default cursor-pointer mr-4">
+                  {link.label}
+                </div>
+                <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-44">
+                  {DATA?.PRODUCTS.map((product, index) => (
+                    <Link href={product.href} key={index}>
+                      <li onClick={handleClick} className="text-wrap cursor-pointer text:md font-semibold h-12 sm:h-14 md:h-16 lg:h-14 xl:h-14 flex items-left justify-center">
+                        <p>{product.name}</p>
+                      </li>
+                    </Link>
+                  ))}
+                </ul>
+              </div>) 
+              
+              )}
+               <div className="dropdown">
             <div tabIndex={0} className="lg:text-nowrap lg:text-md xl:text-lg lg:font-bold text-default cursor-pointer mr-4 left-8">{currentLanguage}</div>
             <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-18 right-05">
               {LANGUAGES.map((lang) => (
