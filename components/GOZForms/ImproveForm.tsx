e0d175dddf90e5d062352eb257880d9f8a4ce79b
unknown <ul0249604@edu.uni.lodz.eu>
2024-08-22 11:31:27 +0200

    added email placholder

--- components/GOZForms/ImproveForm.tsx
+++ components/GOZForms/ImproveForm.tsx
@@ -36,12 +36,12 @@ const ImproveForm = () => {
 
                     <div className="w-full px-3">
                         <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                            htmlFor="grid-last-name">
+                            htmlFor="grid-email">
                             {DATA?.CONTACT_FORM_EMAIL}
                         </label>
                         <input
                             className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                            id="grid-last-name" type="text" 
+                            id="gridemail"  type="email" placeholder="********@*****.**" 
                             {...register('email', { required:   DATA?.CONTACT_FORM_ERROR_MSG })}/>
                         {errors.email && <p className="text-red-500 text-xs italic">{errors.email.message}</p>}
                     </div>
